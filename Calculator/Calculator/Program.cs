7c514982593de3b00353cdd60a7a07654d21aee9
Dshafique <dawudshafique95@gmail.com>
2019-08-20 23:40:50 -0500

    fixed colon error

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -24,11 +24,9 @@ namespace Calculator
                     break;
 
                 case "d":
-                    if (num2!=0)
+                    if (num2 != 0)
                     {
-
                         result = num1 / num2;
-
                     }                    
                     break;
 
@@ -44,34 +42,33 @@ namespace Calculator
         static void Main(string[] args)
         {
             bool endApp = false;
-
             Console.WriteLine("Console Calculator in C#\nDaoud Shafique - V2\r");
             Console.WriteLine("---------------------------------------------\n");
 
             while (!endApp)
             {
-                string num1input = "";
-                string num2input = "";
+                string numInput1 = "";
+                string numInput2 = "";
                 double result = 0;
 
                 Console.Write("type a number, and then press enter: ");
-                num1input = Console.ReadLine();
+                numInput1 = Console.ReadLine();
 
                 double cleanNum1 = 0;
-                while (!double.TryParse(num1input, out cleanNum1))
+                while (!double.TryParse(numInput1, out cleanNum1))
                 {
-                    Console.WriteLine("Wrong input, try again: ");
-                    num1input = Console.ReadLine();
+                    Console.Write("Wrong input, try again: ");
+                    numInput1 = Console.ReadLine();
                 }
 
                 Console.Write("type a number, and then press enter: ");
-                num2input = Console.ReadLine();
+                numInput2 = Console.ReadLine();
 
                 double cleanNum2 = 0;
-                while (!double.TryParse(num2input, out cleanNum2))
+                while (!double.TryParse(numInput2, out cleanNum2))
                 {
-                    Console.WriteLine("Wrong input, try again: ");
-                    num2input = Console.ReadLine();
+                    Console.Write("Wrong input, try again: ");
+                    numInput2 = Console.ReadLine();
                 }
 
                 Console.WriteLine("Choose one of the following: ");
@@ -79,7 +76,7 @@ namespace Calculator
                 Console.WriteLine("\ts - subtract ");
                 Console.WriteLine("\tm - multiply ");
                 Console.WriteLine("\td - divide ");
-                Console.Write("type your choice:");
+                Console.Write("type your choice: ");
 
                 string op = Console.ReadLine();
 
@@ -88,19 +85,19 @@ namespace Calculator
                     result = Calculator.DoOperation(cleanNum1, cleanNum2, op);
                     if (double.IsNaN(result))
                     {
-                        Console.WriteLine("error");
+                        Console.WriteLine("This operation will result in a mathematical error.\n");
                     }
-                    else Console.WriteLine("Your result: {0.0.##}\n", result);
+                    else Console.WriteLine("Your result: {0:0.##}\n", result);
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine("math exception. \nDetails: " + e.Message);
+                    Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
                 }
 
                 Console.WriteLine("---------------------------------------\n");
 
-                Console.Write("Press 'n' and Enter to close the app, or press any other key and enter to continue");
-                if (Console.ReadLine() == "n") endApp = true;
+                Console.Write("Press 'q' and Enter to close the app, or press any other key and enter to continue");
+                if (Console.ReadLine() == "q") endApp = true;
 
                 Console.WriteLine("\n");
             }
